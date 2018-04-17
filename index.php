73303bafb41ed502a41a805c99ef4bd2d7527dbb
Dan Schneider <Dschnd@gmail.com>
2018-04-17 01:46:49 -0500

    Added project to github

---
+++ index.php
@@ -0,0 +1,179 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Crisp Solutions</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/main.css">
+</head>
+<body>
+		<nav>
+			<ul class="desktop-menu">
+				<li><a href="">Home</a></li>
+				<li class="nav-dropdown">
+					<a href="javascript:void(0)" class="dropbtn">Who We Are</a>
+					<div class="nav-dropdown-content">
+						<a href="#">Knowledge & Expertise</a>
+						<a href="#">Communication and Follow-Through</a>
+						<a href="#">Partnership</a>
+					</div>
+				</li>
+				<li><a href="">What We Do</a></li>
+				<li><a href="">Partners</a></li>
+				<li><a href="#contact-section">Get In Touch</a></li>
+			</ul>
+		</nav>
+	<section>
+
+		<div class="home-section">
+			<img src="assets/img/header-mac.png" class="home-section-img" title="Apple Products">
+			<div class="home-section-info">
+				<img src="assets/img/crisp-logo.png" class="crispSolutions-logo" title="Crisp Solutions" alt="Crisp Solutions">
+				<div class="lead"><p>When it just</p><p> needs to work.</p></div>
+			</div>
+
+		</div>
+	</section>
+
+	<section>
+		<div class="about-section about-section-knowledge">
+			<div class="section-overlay">
+				<h2>Knowledge and Expertise</h2>
+				<p class="about-section-descriptions">
+					We are the only Apple Macintosh consultants in the Des Moines Metro area fully certified by Apple. A proud member of the Apple Consultants Network, with the coveted Mobile Technology competency, we can support all of your Apple Macintosh and iOS devices with ease.
+				</p>
+				<img src="" class="apple-consultants-network-logo" title="Apple Consultants Network">
+			</div>
+		</div>
+	</section>
+
+	<section>
+		<div class="about-section about-section-communication">
+			<div class="section-overlay">
+				<h2>Communication </h2><h2>and Follow-Through</h2>
+				<p class="about-section-descriptions">
+					We get it, we are a small business too. At Crisp Solutions we make sure you understand what we are doing and when we do it. We're not your typical IT guys, we can actually communicate in a fashion you can understand.			
+				</p>
+			</div>
+		</div>
+	</section>
+
+	<section>
+		<div class="about-section about-section-partnership">
+			<div class="section-overlay">
+				<h2>Partnership</h2>
+				<p class="about-section-descriptions">
+					We strive to be your first call every-time. Our goal isn't just to be your vendor but a trusted ally and a true partner in your business. We all want to do what we are good at, so let us take care of what we do best, so you can do what you do best!
+				</p>
+			</div>
+		</div>
+	</section>
+
+	<section>
+		<div class="what-we-do-section">
+			<div class="section-overlay">
+				<h2>What We Do</h2>
+				<div class="what-we-do-section-overlay">
+					<img src="" title="Apple Products" alt="Apple Products">
+					<ul>
+						<li>New Device Setup & Installation</li>
+						<li>Disaster Recovery & Backup</li>
+						<li>Monitoring & Management</li>
+						<li>Troubleshooting & Break-fix</li>
+						<li>Networking, Firewalls, and WiFi</li>
+						<li>Virtual CIO</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</section>
+
+
+
+
+
+	<div class="partners-section">
+		<div class="partner-container">
+			<img src="" class="partner-logo" alt="Partner XXX" title="XXX">
+			<p class="partner-description">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			</p>
+			<p class="partner-website">Partner Website <span><img src=""></span></p>
+		</div>
+		<div class="partner-container">
+			<img src="" class="partner-logo" alt="Partner XXX" title="XXX">
+			<p class="partner-description">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			</p>
+			<p class="partner-website">Partner Website <span><img src=""></span></p>
+		</div>
+	</div>
+
+	<div class="contact-section">
+		<div class="section-overlay">
+			<h2>Get In Touch</h2>
+			<p>
+			Need Help? Go Questions? Give us a call, or drop us a line. We promise we will get back to you in less than a business day.
+			</p>
+			<div class="contact-container">
+				<div class="contact-form-container">
+					<form>
+						<label for="business">Business:</label>
+						<input type="text" name="inBusiness">
+
+						<label for="name">Full Name:</label>
+						<input type="text" name="inFullName">
+
+						<label for="email">Email:</label>
+						<input type="text" name="inEmail">
+
+						<label for="phone">Phone:</label>
+						<input type="text" name="inPhone">
+
+						<label for="message">Message:</label>
+						<input type="text" name="inPhone">
+
+						<input type="submit" name="submitForm" value="Submit">
+						<input type="reset" name="resetForm" value="Reset">
+					</form>
+				</div>
+				<div class="contact-info-container">
+					<div class="contact-info-address">
+						<h4>Address:</h4>
+						<p class="contact-info-address-line">3123 Holcomb Ave</p>
+						<p class="contact-info-address-line">Des Moines, IA 50310</p>
+						<p class="contact-info-address-line">USA</p>
+					</div>
+					<div class="contact-info-email">
+						<h4>Email:</h4>
+						<p>info@crispsolutions.net</p>
+					</div>
+					<div class="contact-info-phone">
+						<h4>Phone:</h4>
+						<p>(515) 423-0160</p>
+					</div>
+					<div class="contact-info-social">
+						<h4>Social:</h4>
+						<img src="" alt="Facebook" title="Facebook">
+						<img src="" alt="LinkedIn" title="LinkedIn">
+					</div>
+
+					<div class="request-buttons">
+						<button>Request A Quote</button>
+						<button>Request A Booking</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+</body>
+</html>
